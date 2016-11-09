3c6e5d7ce7772df127fb46f3321bf8c6e33adf5d
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2016-11-09 02:48:48 +0100

    Init commit

---
+++ src/document.ts
@@ -0,0 +1,133 @@
+import { workspace, window, commands, TextDocumentContentProvider,
+    Event, Uri, TextDocumentChangeEvent, ViewColumn, EventEmitter,
+    TextDocument, Disposable } from "vscode";
+import * as path from "path";
+import fileUrl = require("file-url");
+import { SourceType } from "./extension";
+
+export class HtmlDocumentView {
+    private provider: HtmlDocumentContentProvider;
+    private registrations: Disposable[] = [];
+    private title: string;
+    private previewUri: Uri;
+    private doc: TextDocument;
+
+    constructor(document: TextDocument) {
+        this.doc = document;
+        this.title = `Preview: '${path.basename(window.activeTextEditor.document.fileName)}'`;
+        this.provider = new HtmlDocumentContentProvider(this.doc);
+        this.registrations.push(workspace.registerTextDocumentContentProvider("html-preview", this.provider));
+        this.previewUri = Uri.parse(`html-preview://preview/${this.title}`);
+        this.registerEvents();
+    }
+    
+    public get uri(): Uri {
+        return this.previewUri;
+    }
+
+    private registerEvents() {
+        this.registrations.push(workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
+            if (!this.visible) {
+                return;
+            }
+            if (e.document === this.doc) {
+                this.provider.update(this.previewUri);
+            }
+        }));
+    }
+
+    private get visible(): boolean {
+        for (let i in window.visibleTextEditors) {
+            if (window.visibleTextEditors[i].document.uri === this.previewUri) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public executeToggle(column: ViewColumn) {
+        if (this.visible) {
+            window.showTextDocument(this.doc, column);
+            this.visible = false;
+        } else {
+            this.execute(column);
+        }
+    }
+
+    public executeSide(column: ViewColumn) {
+        this.execute(column);
+    }
+
+    private execute(column: ViewColumn) {
+        commands.executeCommand("vscode.previewHtml", this.previewUri, column).then((success) => {
+        }, (reason) => {
+            console.warn(reason);
+            window.showErrorMessage(reason);
+        });
+    }
+    
+    public dispose() {
+        for (let i in this.registrations) {
+            this.registrations[i].dispose();
+        }
+    }
+}
+
+class HtmlDocumentContentProvider implements TextDocumentContentProvider {
+    private _onDidChange = new EventEmitter<Uri>();
+    private doc: TextDocument;
+
+    constructor(document: TextDocument) {
+        this.doc = document;
+    }
+
+    public provideTextDocumentContent(uri: Uri): string {
+        return this.createHtmlSnippet();
+    }
+
+    get onDidChange(): Event<Uri> {
+        return this._onDidChange.event;
+    }
+
+    public update(uri: Uri) {
+        console.log('Update file');
+        this._onDidChange.fire(uri);
+    }
+
+    private createHtmlSnippet(): string {
+        if (this.doc.languageId !== "cmacc") {
+            return this.errorSnippet("Active editor doesn't show a cmacc.");
+        }
+        return this.preview();
+    }
+
+    private errorSnippet(error: string): string {
+        return `
+                <body>
+                    ${error}
+                </body>`;
+    }
+
+    private render(): string {
+
+        var cmacc = require('cmacc-compiler');
+        var path = require('path');
+
+        var text = this.doc.getText();
+        var file = this.doc.fileName
+
+        var options = {
+            path: 'file://' + path.dirname(file)
+        }
+
+        var ast = cmacc.compile(text, options)
+        var md = cmacc.resolve(ast);  
+        var html = cmacc.marked(md); 
+
+        return html
+    }
+
+    public preview(): string {
+        return this.render();
+    }
+}
---
+++ src/extension.ts
@@ -0,0 +1,106 @@
+"use strict";
+import { window, ExtensionContext, commands, Uri,
+    ViewColumn, TextDocument, TextEditor } from "vscode";
+import * as uuid from "node-uuid";
+import { HtmlDocumentView } from "./document";
+
+export enum SourceType {
+    SCRIPT,
+    STYLE
+}
+let viewManager: ViewManager;
+export function activate(context: ExtensionContext) {
+    viewManager = new ViewManager();
+
+    context.subscriptions.push(
+        commands.registerCommand("cmacc.previewToSide", () => {
+            viewManager.previewToSide();
+        }),
+        commands.registerCommand("cmacc.preview", () => {
+            viewManager.preview();
+        })
+    );
+}
+
+export function deactivate() {
+    viewManager.dispose();
+}
+
+class ViewManager {
+    private idMap: IDMap = new IDMap();
+    private fileMap: Map<string, HtmlDocumentView> = new Map();
+
+    private sendHTMLCommand(displayColumn: ViewColumn, doc: TextDocument, toggle: boolean = false) {
+        let id: string;
+        let htmlDoc: HtmlDocumentView;
+        if (!this.idMap.hasUri(doc.uri)) {
+            htmlDoc = new HtmlDocumentView(doc);
+            id = this.idMap.add(doc.uri, htmlDoc.uri);
+            this.fileMap.set(id, htmlDoc);
+        } else {
+            id = this.idMap.getByUri(doc.uri);
+            htmlDoc = this.fileMap.get(id);
+        }
+        if (toggle || htmlDoc.uri === doc.uri) {
+            htmlDoc.executeToggle(displayColumn);
+        } else {
+            htmlDoc.executeSide(displayColumn);
+        }
+    }
+
+    public previewToSide() {
+        let displayColumn: ViewColumn;
+        switch (window.activeTextEditor.viewColumn) {
+            case ViewColumn.One:
+                displayColumn = ViewColumn.Two;
+                break;
+            case ViewColumn.Two:
+            case ViewColumn.Three:
+                displayColumn = ViewColumn.Three;
+                break;
+        }
+        this.sendHTMLCommand(displayColumn,
+            window.activeTextEditor.document);
+    }
+
+    public preview() {
+        // activeTextEditor does not exist when triggering on a html preview
+        this.sendHTMLCommand(window.activeTextEditor.viewColumn,
+            window.activeTextEditor.document, true);
+    }
+    
+    public dispose() {
+        let values = this.fileMap.values()
+        let value: IteratorResult<HtmlDocumentView> = values.next();
+        while (!value.done) {
+            value.value.dispose();
+            value = values.next();
+        }
+    }
+}
+
+class IDMap {
+    private map: Map<[Uri, Uri], string> = new Map();
+
+    public getByUri(uri: Uri) {
+        let keys = this.map.keys()
+        let key: IteratorResult<[Uri, Uri]> = keys.next();
+        while (!key.done) {
+            if (key.value.indexOf(uri) > -1) {
+                return this.map.get(key.value);
+            }
+            key = keys.next();
+        }
+        return null;
+    }
+
+    public hasUri(uri: Uri) {
+        return this.getByUri(uri) !== null;
+    }
+
+    public add(uri1: Uri, uri2: Uri) {
+        let id = uuid.v4();
+        this.map.set([uri1, uri2], id);
+        return id;
+    }
+}
